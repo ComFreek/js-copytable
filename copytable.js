ecd5c229495d20599b9431c0969b4717227ffce0
ComFreek <programmer-comfreek@hotmail.com>
2012-09-07 11:22:59 +0200

    Replaced whitespaces by tabs

--- copytable.js
+++ copytable.js
@@ -6,28 +6,28 @@
   * @return Return TRUE on success, otherwise FALSE.
   */
 function copyTableIntoClipboard(tbl) {
-    if (!tbl) {
+	if (!tbl) {
 		return false;
 	}
 	
 	var str = "";
 
-    for (var y=0, rowsLen=tbl.rows.length; y<rowsLen; y++) {
-        var row = tbl.rows[y];
+	for (var y=0, rowsLen=tbl.rows.length; y<rowsLen; y++) {
+		var row = tbl.rows[y];
 
-        for (var x=0, cellsLen=row.cells.length; x<cellsLen; x++) {
-            var content = row.cells[x].innerText;
-            
-            str += content;
-            
-            if (x != cellsLen-1) {
-                str += "\t";
-            }
-        }
-        
-        if (y != rowsLen-1) {
-            str += "\n";
-        }
-    }
-    return window.clipboardData.setData("Text", str);
+		for (var x=0, cellsLen=row.cells.length; x<cellsLen; x++) {
+			var content = row.cells[x].innerText;
+			
+			str += content;
+			
+			if (x != cellsLen-1) {
+				str += "\t";
+			}
+		}
+		
+		if (y != rowsLen-1) {
+			str += "\n";
+		}
+	}
+	return window.clipboardData.setData("Text", str);
 }
